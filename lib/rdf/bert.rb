c4693fd1f667d1b41e7fe7e4a8b7f28aa5bed98d
Arto Bendiken <arto@bendiken.net>
2010-10-20 17:25:57 +0200

    Reimplemented RDF::BERT.decode to use the new RDF/BERT serialization format.

--- lib/rdf/bert.rb
+++ lib/rdf/bert.rb
@@ -20,9 +20,9 @@ module RDF
     # @return [String]
     def self.serialize(value, options = {})
       case value
-        when Array      then RDF::Statement.new(*value).to_bert
+        when Array      then RDF::Statement(*value).to_bert
         when RDF::Value then value.to_bert
-        else RDF::Literal.new(value, options).to_bert
+        else RDF::Literal(value, options).to_bert
       end
     end
 
@@ -37,7 +37,23 @@ module RDF
     # @param  [Object] value
     # @return [RDF::Value]
     def self.unserialize(value)
-      raise NotImplementedError, "RDF/BERT unserialization not yet implemented" # TODO
+      case value
+        when :f, :t, FalseClass, TrueClass, Integer, Float
+          RDF::Literal(value)
+        when Array, ::BERT::Tuple
+          case tag = value.first
+            when :'3' then RDF::Statement(*value[1..3].map { |term| unserialize(term) })
+            when :'4' then RDF::Statement(*value[1..3].map { |term| unserialize(term) }, :context => unserialize(value[4]))
+            when :'?' then RDF::Query::Variable.new(value[1])
+            when :':' then RDF::Node(value[1])
+            when :'<' then RDF::URI(value[1])
+            when :'"' then RDF::Literal(value[1])
+            when :'@' then RDF::Literal(value[1], :language => value[2])
+            when :'^' then RDF::Literal(value[1], :datatype => value[2])
+          end
+        else
+          raise ArgumentError, "invalid RDF/BERT value: #{value.inspect}"
+      end
     end
   end
 end
